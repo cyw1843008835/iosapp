07f2550f4fd59a897327c5d1d63231088726baa6
chen <chen@gmsil.com>
2019-12-29 14:35:23 +0900

    Initial Commit

---
+++ demo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo2
+//
+//  Created by yw c on 2019/12/29.
+//  Copyright © 2019 yw c. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
